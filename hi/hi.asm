b2fd736e8ee97018adea8970ea6ca45b5dbd0cf2
Maath-Lima <math3226@gmail.com>
2025-05-28 21:22:44 -0300

    push and stack playtime

--- hi/hi.asm
+++ hi/hi.asm
@@ -10,14 +10,16 @@ greet: db "Hi", NEWLINE
 
 section .text
 _start:
+push greet
 call .print ; jump to label .print
+pop rbp     ; stack pop, rbp register is general-purpose, but it mantains the base of the stack
 .exit:
     mov rdi, 0
     mov rax, SYS_exit
     syscall
 .print:
     mov rdi, STDOUT         
-    mov rsi, greet
+    mov rsi, [rsp + 8] ; the "Hi" string is located 8 bytes below the top of the stack
     mov rdx, 3
     mov rax, SYS_write
     syscall
